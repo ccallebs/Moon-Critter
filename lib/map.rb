85e37a29076f3fb56f78749b90aa60067abf67bc
Chuck Callebs <cacallebs@gmail.com>
2013-05-16 22:40:57 -0500

    make tiles public on map class.

--- lib/map.rb
+++ lib/map.rb
@@ -2,13 +2,14 @@ require 'gosu'
 require './lib/map_tile'
 
 class Map
-  attr_accessor :window
+  attr_accessor :window, :tiles
 
   def initialize(window)
     @window = window
     @char_map = []
 
-    populate_tiles
+    populate_char_map
+    build_tiles
   end
 
   def draw
@@ -17,14 +18,18 @@ class Map
 
     horizontal_tiles.times do |x|
       vertical_tiles.times do |y|
-        MapTile.new(@window, @char_map[x][y], x * 50, y * 50).draw
+        if @tiles[x][y].nil?
+          MapTile.new(@window, @char_map[x][y], x * 50, y * 50).draw
+        else
+          @tiles[x][y].draw
+        end
       end
     end
   end
 
 private 
 
-  def populate_tiles
+  def populate_char_map
     text = File.open("./assets/small_farm.dat").read
 
     line_counter = 0
@@ -39,6 +44,10 @@ private
 
       line_counter += 1
     end
+  end
+
+  def build_tiles
+    @tiles = Array.new(@char_map.length, Array.new(@char_map[0].length))
   end
 
 end
