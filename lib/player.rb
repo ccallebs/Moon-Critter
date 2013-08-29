eb72138a0ef7eee35d43c89b4add1f68bd0b3111
Chuck Callebs <cacallebs@gmail.com>
2013-08-28 23:51:35 -0500

    add PlayerPosition class. refactor Player class.

--- lib/player.rb
+++ lib/player.rb
@@ -1,5 +1,6 @@
 require 'gosu'
 require './lib/game_window'
+require './lib/player_position'
 
 class Player
   attr_accessor :window, :image, :movement_counter
@@ -9,9 +10,7 @@ class Player
   def initialize(window)
     self.window = window
     @image = Gosu::Image.new(@window, 'assets/player.png', false)
-    @x = 25
-    @y = 25
-    @angle = 0.0
+    @position = PlayerPosition.new(x: 25, y: 25, angle: 0.0)
     @movement_counter = 0
   end
 
@@ -20,75 +19,18 @@ class Player
     return unless @movement_counter % 5 == 0
 
     if @window.button_down? Gosu::KbRight
-      turn_right
-      move_right
+      @position.button_right
     elsif @window.button_down? Gosu::KbLeft
-      turn_left
-      move_left
+      @position.button_left
     elsif @window.button_down? Gosu::KbDown
-      turn_down
-      move_down
+      @position.button_down
     elsif @window.button_down? Gosu::KbUp
-      turn_up
-      move_up
+      @position.button_up
     end
   end
 
   def draw
-    @image.draw_rot(@x, @y, 2, @angle)
-  end
-
-private
-
-  # Turn Methods
-  def turn_right
-    @angle = 90.0
-  end
-
-  def turn_left
-    @angle = 270.0
-  end
-
-  def turn_up
-    @angle = 0.0
-  end
-
-  def turn_down
-    @angle = 180.0
-  end
-
-  # Movement Methods
-
-  def move_right
-    new_x = @x + 50 
-    unless new_x == GameWindow::WIDTH + 25
-      @x = new_x
-      puts "moving right."
-    end
-  end
-
-  def move_left
-    new_x = @x - 50
-    unless new_x == -25
-      @x = new_x
-      puts "moving left."
-    end
-  end
-
-  def move_up
-    new_y = @y - 50
-    unless new_y == -25
-      @y = new_y
-      puts "moving up."
-    end
-  end
-
-  def move_down
-    new_y = @y + 50
-    unless new_y == GameWindow::HEIGHT + 25
-      @y += 50
-      puts "moving down."
-    end
+    @image.draw_rot(@position.x, @position.y, 2, @position.angle)
   end
 
 end
--- lib/player_position.rb
+++ lib/player_position.rb
@@ -1,6 +1,68 @@
-class PlayerPosition
-  attr_accessor :tiles, :position_x, :position_y
+require 'gosu'
+require './lib/game_window'
 
-  def initialize(tiles, position_x, position_y)
+class PlayerPosition
+  attr_accessor :x, :y, :angle
+
+  def initialize(options = {})
+    @x = options[:x]
+    @y = options[:y]
+    @angle = options[:angle]
+  end
+
+  # Defining button_right, button_left, button_up, button_down
+  [:right, :left, :up, :down].each do |direction|
+    define_method("button_#{direction}") do 
+      send("turn_#{direction}")
+      send("move_#{direction}")
+    end
+  end
+
+private
+
+  # Turn Methods
+  def turn_right
+    @angle = 90.0
+  end
+
+  def turn_left
+    @angle = 270.0
+  end
+
+  def turn_up
+    @angle = 0.0
+  end
+
+  def turn_down
+    @angle = 180.0
+  end
+
+  # Movement Methods
+  def move_right
+    new_x = @x + 50 
+    unless new_x == GameWindow::WIDTH + 25
+      @x = new_x
+    end
+  end
+
+  def move_left
+    new_x = @x - 50
+    unless new_x == -25
+      @x = new_x
+    end
+  end
+
+  def move_up
+    new_y = @y - 50
+    unless new_y == -25
+      @y = new_y
+    end
+  end
+
+  def move_down
+    new_y = @y + 50
+    unless new_y == GameWindow::HEIGHT + 25
+      @y += 50
+    end
   end
 end
